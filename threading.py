c5797f093be60549c024e42757537b43244d9148
Mohamed Aboshosha <maboshosha301@gmail.com>
2017-12-17 12:19:00 +0200

    Update threading.py

--- threading.py
+++ threading.py
@@ -5,8 +5,6 @@ class my_thread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
-    ex=True
-
     def __init__(self):
         super(my_thread, self).__init__()
         self._stop_event = threading.Event()
@@ -15,7 +13,6 @@ class my_thread(threading.Thread):
         thread.start()  
 
     def stop(self):
-        ex=False
         self._stop_event.set()
 
     def stopped(self):
@@ -25,9 +22,8 @@ class my_thread(threading.Thread):
         while not self.stopped():
             # Do something
             print('Doing something imporant in the background')
-            time.sleep(5)
 
 
 t = my_thread()
-time.sleep(25)
+time.sleep(1)
 t.stop()
