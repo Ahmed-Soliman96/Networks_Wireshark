5a2d72fc2a682ea9504d75a9a82a4c9e161f5c3c
Mohamed Aboshosha <maboshosha301@gmail.com>
2017-12-17 12:50:42 +0200

    Update threading.py

--- threading.py
+++ threading.py
@@ -5,6 +5,8 @@ class my_thread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
+    pkts=[] # if this thread is for GUI, then the pkts variable is not needed
+
     def __init__(self):
         super(my_thread, self).__init__()
         self._stop_event = threading.Event()
@@ -22,6 +24,9 @@ class my_thread(threading.Thread):
         while not self.stopped():
             # Do something
             print('Doing something imporant in the background')
+
+    def return_packets(): # if this thread is for GUI, then the return_packets function is not needed
+        return my_thread.pkts
 
 
 t = my_thread()
