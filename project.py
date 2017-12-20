7b1d02845c063a694f96e44322efdfbd6528a840
Ahmed Soliman <1300113@eng.asu.edu.eg>
2017-12-20 14:29:59 +0200

    Updating functions

--- project.py
+++ project.py
@@ -4,10 +4,19 @@ import threading_fun
 
 pkts = []
 pktslst = {}
+intfces = {}
+
+"""List of interface to choose which one to sniff from"""
+def get_interfaces():
+    global intfces
+    lst = get_windows_if_list()
+    for i in range(len(lst)):
+        intfces[str(i+1)] = get_windows_if_list()[i]['name']
+    return intfces
 
 """INTO THREAD"""
 """When clicking start bottun to sniff packets ||| Also can be used for filtering packets by setting the 'pktsfilter' as we want"""
-def sniff_packets(pktscount = 0, interface = None, pktsfilter = None):
+def sniff_packets(pktscount = 0, interface = conf.iface, pktsfilter = None): #conf.iface == connected interface
     global pkts
     pkts = sniff(count = pktscount,iface = interface, filter = pktsfilter)
     return pkts
@@ -39,8 +48,6 @@ def get_info(pkt):
 
 """Creates a dectionary of what to be viewed in the GUI of Wireshark packets' list"""
 def make_packetslist(pkts):
-    #global lst
-    #lst.append([len(lst)+1]+get_info(pkt))
     global pktslst
     for count in range(len(pkts)):
         pktslst[str(count+1)] = [str(count+1)]+get_info(pkts[count])
@@ -49,17 +56,16 @@ def make_packetslist(pkts):
 """To save a '.pcap' file of the packets"""
 def save_file(flname, pkts, app=False):
     wrpcap(filename=flname, pkt=pkts, append=app)
-#####################################
 
-def runcode():
-    sniff_packets(pktscount=10,pktsfilter="") #To be used used on clicking start #Assume 10 packets only and no filters as example, It works
-    save_file("mypackets.pcap", pkts)
-    print(make_packetslist(pkts)) #Dectionary to be used in the packets' view window
-    """
+#####################################
+t = threading_fun.my_thread() #Try to run in an independent thread, but still doesn't work
+t.run(sniff_packets,[0]) #To be used used on clicking start #Assume 10 packets only and no filters as example, It works
+print(make_packetslist(pkts)) #Dectionary to be used in the packets' view window
+save_file("mypackets.pcap", pkts)
+
+"""
     for i in range(len(pkts)):
         print(get_hexa(pkts[i]))
         display_packetdata(pkts[i])
-    """
-t = threading_fun.my_thread(runcode()) #Try to run in an independent thread, but still doesn't work
-#t.start()
+"""
 #time.sleep(1)
--- threading_fun.py
+++ threading_fun.py
@@ -1,22 +1,23 @@
 import threading
 import time
 
+
 class my_thread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
-    pkts=[] 
+    pkts = []
 
     def __init__(self):
         super(my_thread, self).__init__()
         self._stop_event = threading.Event()
         thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                              #starts by default as soon as called
+        thread.daemon = True  # Daemonize thread
+        thread.start()  # starts by default as soon as called
 
     """
     #########  To call a function in the thread  #########
-    
+
         def __init__(self, target, *args):
             self._target = target
             self._args = args
@@ -31,13 +32,25 @@ class my_thread(threading.Thread):
 
     def stopped(self):
         return self._stop_event.is_set()
-    def run(self):
-        """ Method that runs forever """
+
+    def run(self, funct, args):
+        """
+        Method that runs forever
+
+        ## Arguments
+        + funct : A function object to be called
+        + args : List of arguments
+
+        ## Usage
+        run(print_Hello, ['hello', 'world'])
+        will attempt to call print_hello('hello', 'world')
+        """
         while not self.stopped():
             # Do something
-            print('Doing something imporant in the background')
+            funct(args[0])  # , args[1])
+            # print('Doing something imporant in the background')
 
-    def return_packets(): # this function is to retrive the packets outside the thread
+    def return_packets():  # this function is to retrive the packets outside the thread
         return my_thread.pkts
 
 """
